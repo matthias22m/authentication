fec6ab32f80c006c136c50236660e7278b7b4ec5
Matthias Mulugeta <matthias22m@gmail.com>
2024-12-15 15:14:40 +0300

    export UsersService and add mock user data with findByName method

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
 
+export type User = {
+    userId : number,
+    username : string,
+    password : string,
+}
+
+// write mock data  for users
+export const users: User[] = [
+    {
+        userId: 1,
+        username: 'bereket',
+        password: 'bereket123',
+    },
+    {
+        userId: 2,
+        username: 'henok',
+        password: 'henok123',
+    },
+];
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    async findByName(username: string): Promise<User | undefined> {
+        return users.find(user => user.username === username);
+    }
+}
